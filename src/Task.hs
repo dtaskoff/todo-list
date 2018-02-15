40ab9961ee09b53553e679905551fba3ae8ac2bb
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-15 15:39:00 +0200

    Implement a handler for DELETE /task/:id

--- src/Task.hs
+++ src/Task.hs
@@ -30,6 +30,21 @@ getTaskIDR i = do
 --    Just task -> returnJson task
 --    Nothing   -> notFound
 
+deleteTaskIDR :: Int -> Handler Value
+-- deleteTaskIDR i = do
+deleteTaskIDR i = do
+  tasks' <- getsYesod tasks
+  mtask <- liftIO $ modifyMVar tasks' (pure . removeTask i)
+  maybe notFound returnJson mtask
+
+-- | Given an index `i` and Tasks gives a tuple with all Tasks without the first
+-- one having such an index and Just that Task, if such Task exists, else Nothing
+removeTask :: Int -> Tasks -> (Tasks, Maybe Task)
+removeTask i tasks =
+  case break ((== i) . tid) tasks of   -- ^ breaks the list of tasks into two halves
+    (ts, t:ts') -> (ts ++ ts', Just t) -- ^ where `t` is the first Task in the list that has an index `i`
+    (ts, []) -> (ts, Nothing)
+
 -- | A simple handler, that just outputs in the console the JSON body of the POST request
 -- Note: requireJsonBody succeeds only if the passed JSON is correct, e.g. matches the
 -- FromJSON instance for Task
