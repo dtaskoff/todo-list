19f232d23af7d26e815d6a8ae85005e6493a23a9
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-15 16:58:46 +0200

    Move TaskPOST and TaskPUT to separate modules to enable the use of same names for record fields

--- src/Task.hs
+++ src/Task.hs
@@ -21,24 +21,8 @@ matchesID i = (== i) . tid
 
 type Tasks = [Task]
 
--- | Change only these fields, which are not Nothing in taskPUT
-updateTask :: Task -> TaskPUT -> Task
-updateTask task taskPUT = task
-  { title = maybe (title task) id (titlePUT taskPUT)
-  , description = maybe (description task) id (descriptionPUT taskPUT)
-  , status = maybe TODO id (statusPUT taskPUT)
-  }
-
 addTask :: Task -> Tasks -> Tasks
 addTask = (:)
-
--- | We're making this as a separate datatype for PUT requests, because we want to make sure
--- that using `Task` will enforce setting all fields
-data TaskPUT = TaskPUT
-  { titlePUT       :: Maybe String
-  , descriptionPUT :: Maybe String
-  , statusPUT      :: Maybe Status
-  } deriving Generic
 
 -- | Given an index `i` and Tasks gives a tuple with all Tasks without the first
 -- one having such an index and Just that Task, if such Task exists, else Nothing
@@ -48,33 +32,11 @@ removeTask i tasks =
     (ts, t:ts') -> (ts ++ ts', Just t) -- ^ where `t` is the first Task in the list that has an index `i`
     (ts, []) -> (ts, Nothing)
 
--- | We're making this as a separate datatype for POST requests, because we want to make sure
--- that using `Task` will enforce setting all fields
-data TaskPOST = TaskPOST
-  { titlePOST       :: String
-  , descriptionPOST :: String
-  , statusPOST      :: Maybe Status
-  } deriving Generic
-
-createTaskFromPOST :: Int -> TaskPOST -> Task
-createTaskFromPOST i taskPOST = Task
-  { tid = i
-  , title = titlePOST taskPOST
-  , description = descriptionPOST taskPOST
-  , status = maybe TODO id (statusPOST taskPOST)
-  }
-
 data Status = TODO | Done
   deriving (Generic, Show)
 
 instance FromJSON Task
 instance ToJSON Task
-
-instance FromJSON TaskPUT
-instance ToJSON TaskPUT
-
-instance FromJSON TaskPOST
-instance ToJSON TaskPOST
 
 instance FromJSON Status
 instance ToJSON Status
---
+++ src/TaskPOST.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric #-}
+module TaskPOST where
+
+import GHC.Generics
+import Task (Task(Task), Status(..))
+import Yesod.Core.Json
+
+import qualified Task as T
+-- ^ because Task exports title, description and status
+
+-- | We're making this as a separate datatype for POST requests, because we want to make sure
+-- that using `Task` will enforce setting all fields
+data TaskPOST = TaskPOST
+  { title       :: String
+  , description :: String
+  , status      :: Maybe Status
+  } deriving Generic
+
+instance FromJSON TaskPOST
+instance ToJSON TaskPOST
+
+createTaskFromPOST :: Int -> TaskPOST -> Task
+createTaskFromPOST i taskPOST = Task
+  { T.tid = i
+  , T.title = title taskPOST
+  , T.description = description taskPOST
+  , T.status = maybe TODO id (status taskPOST)
+  }
