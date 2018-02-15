ad54749d9259084c9f5aefc381053d533078fadc
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-15 16:35:34 +0200

    Refactor: rearrange some parts of the code

--- src/TaskHandlers.hs
+++ src/TaskHandlers.hs
@@ -7,39 +7,11 @@ import Task
 import Yesod.Core
 
 
-getTasksYesod :: Handler Tasks
--- getTasksYesod = liftIO . readMVar =<< getsYesod tasks
-getTasksYesod = do
-  tasksMVar <- getsYesod tasks
-  liftIO $ readMVar tasksMVar
-
 getTaskR :: Handler Value
 -- getTaskR = returnJson =<< getTasksYesod
 getTaskR = do
   tasks' <- getTasksYesod
   returnJson tasks'
-
-getTaskIDR :: Int -> Handler Value
--- getTaskIDR i = maybe notFound returnJson . find (matchesID i) =<< getTasksYesod
-getTaskIDR i = do
-  tasks' <- getTasksYesod
-  let mtask = find (matchesID i) tasks'
-  maybe notFound returnJson mtask
---  case mtask of
---    Just task -> returnJson task
---    Nothing   -> notFound
-
--- | Returns the next index and updates it (increments it by one)
-getNextIndex :: Handler Int
-getNextIndex = do
-  nextIndexMVar <- getsYesod nextIndex
-  liftIO $ modifyMVar nextIndexMVar (\i -> pure (i + 1, i))
-
-deleteTaskIDR :: Int -> Handler Value
-deleteTaskIDR i = do
-  tasksMVar <- getsYesod tasks
-  mtask <- liftIO $ modifyMVar tasksMVar (pure . removeTask i)
-  maybe notFound returnJson mtask
 
 -- | A simple handler, that just outputs in the console the JSON body of the POST request
 -- Note: requireJsonBody succeeds only if the passed JSON is correct, e.g. matches the
@@ -53,6 +25,22 @@ postTaskR = do
   tasksMVar <- getsYesod tasks
   liftIO $ modifyMVar_ tasksMVar (pure . addTask task)
   returnJson task
+
+getTaskIDR :: Int -> Handler Value
+-- getTaskIDR i = maybe notFound returnJson . find (matchesID i) =<< getTasksYesod
+getTaskIDR i = do
+  tasks' <- getTasksYesod
+  let mtask = find (matchesID i) tasks'
+  maybe notFound returnJson mtask
+--  case mtask of
+--    Just task -> returnJson task
+--    Nothing   -> notFound
+
+deleteTaskIDR :: Int -> Handler Value
+deleteTaskIDR i = do
+  tasksMVar <- getsYesod tasks
+  mtask <- liftIO $ modifyMVar tasksMVar (pure . removeTask i)
+  maybe notFound returnJson mtask
 
 putTaskIDR :: Int -> Handler Value
 putTaskIDR i = do
