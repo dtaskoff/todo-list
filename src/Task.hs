0debdae17a1590e1b8b95d248d754ae4797450ad
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-15 15:13:19 +0200

    Implement a handler for GET /task

--- src/Task.hs
+++ src/Task.hs
@@ -5,6 +5,12 @@ import Types
 import Yesod.Core
 
 
+getTaskR :: Handler Value
+-- getTaskR = returnJson =<< getsYesod tasks
+getTaskR = do
+  tasks' <- getsYesod tasks
+  returnJson tasks'
+
 -- | A simple handler, that just outputs in the console the JSON body of the POST request
 -- Note: requireJsonBody succeeds only if the passed JSON is correct, e.g. matches the
 -- FromJSON instance for Task
