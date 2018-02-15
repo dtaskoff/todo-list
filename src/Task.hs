e4e89f432ee4ac0db47981cbb68420f195b4776a
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-15 15:48:53 +0200

    Refactor: move pure Task stuff into Task and Task handlers into TaskHandlers

--- src/Task.hs
+++ src/Task.hs
@@ -1,54 +1,39 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Task where
 
-import Control.Concurrent.MVar
-import Data.List (find)
-import Foundation
-import Types
-import Yesod.Core
+import GHC.Generics
+import Yesod.Core.Json
 
 
-getTasksYesod :: Handler [Task]
--- getTasksYesod = liftIO . readMVar =<< getsYesod tasks
-getTasksYesod = do
-  tasksMVar <- getsYesod tasks
-  liftIO $ readMVar tasksMVar
+{- The DeriveGeneric pragma, and "deriving Generic" below
+ - are needed to avoid implementing the FromJSON and ToJSON
+ - instances ourselves
+ -}
+data Task = Task
+  { tid         :: Int
+  , title       :: String
+  , description :: String
+  , status      :: Status
+  } deriving (Generic, Show)
 
-getTaskR :: Handler Value
--- getTaskR = returnJson =<< getTasksYesod
-getTaskR = do
-  tasks' <- getTasksYesod
-  returnJson tasks'
+type Tasks = [Task]
 
-getTaskIDR :: Int -> Handler Value
--- getTaskIDR i = maybe notFound returnJson . find ((== i) . tid) =<< getTasksYesod
-getTaskIDR i = do
-  tasks' <- getTasksYesod
-  let mtask = find ((== i) . tid) tasks'
-  maybe notFound returnJson mtask
---  let mtask = find ((== i) . tid) tasks'
---  case mtask of
---    Just task -> returnJson task
---    Nothing   -> notFound
-
-deleteTaskIDR :: Int -> Handler Value
--- deleteTaskIDR i = do
-deleteTaskIDR i = do
-  tasks' <- getsYesod tasks
-  mtask <- liftIO $ modifyMVar tasks' (pure . removeTask i)
-  maybe notFound returnJson mtask
+matchesID :: Int -> Task -> Bool
+matchesID i = (== i) . tid
 
 -- | Given an index `i` and Tasks gives a tuple with all Tasks without the first
 -- one having such an index and Just that Task, if such Task exists, else Nothing
 removeTask :: Int -> Tasks -> (Tasks, Maybe Task)
 removeTask i tasks =
-  case break ((== i) . tid) tasks of   -- ^ breaks the list of tasks into two halves
+  case break (matchesID i) tasks of   -- ^ breaks the list of tasks into two halves
     (ts, t:ts') -> (ts ++ ts', Just t) -- ^ where `t` is the first Task in the list that has an index `i`
     (ts, []) -> (ts, Nothing)
 
--- | A simple handler, that just outputs in the console the JSON body of the POST request
--- Note: requireJsonBody succeeds only if the passed JSON is correct, e.g. matches the
--- FromJSON instance for Task
-postTaskR :: Handler ()
-postTaskR = do
-  task <- requireJsonBody
-  liftIO $ print (task :: Task)
+data Status = TODO | Done
+  deriving (Generic, Show)
+
+instance FromJSON Task
+instance ToJSON Task
+
+instance FromJSON Status
+instance ToJSON Status
