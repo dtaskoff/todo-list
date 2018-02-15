9b5319925be598b7e1b61d8973e14a04fc4cddaa
Daniel Taskoff <daniel.taskoff@chaosgroup.com>
2018-02-15 14:58:27 +0200

    Implement a simple handler for POST /task

---
+++ src/Task.hs
@@ -0,0 +1,14 @@
+module Task where
+
+import Foundation
+import Types
+import Yesod.Core
+
+
+-- | A simple handler, that just outputs in the console the JSON body of the POST request
+-- Note: requireJsonBody succeeds only if the passed JSON is correct, e.g. matches the
+-- FromJSON instance for Task
+postTaskR :: Handler ()
+postTaskR = do
+  task <- requireJsonBody
+  liftIO $ print (task :: Task)
