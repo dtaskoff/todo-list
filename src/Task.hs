8afbdcda84574a9d6f2b231d590813a829d29a96
Daniel Taskoff <daniel.taskoff@gmail.com>
2018-02-17 09:25:56 +0200

    Rewrite putTaskIDR to use our database

--- src/Task.hs
+++ src/Task.hs
@@ -28,18 +28,4 @@ Task json
 entityTaskToTask :: Entity Task -> Task
 entityTaskToTask (Entity key task) = task
 
-matchesID :: Int -> Task -> Bool
-matchesID i = (== i) . taskTid
-
 type Tasks = [Task]
-
-addTask :: Task -> Tasks -> Tasks
-addTask = (:)
-
--- | Given an index `i` and Tasks gives a tuple with all Tasks without the first
--- one having such an index and Just that Task, if such Task exists, else Nothing
-removeTask :: Int -> Tasks -> (Tasks, Maybe Task)
-removeTask i tasks =
-  case break (matchesID i) tasks of   -- ^ breaks the list of tasks into two halves
-    (ts, t:ts') -> (ts ++ ts', Just t) -- ^ where `t` is the first Task in the list that has an index `i`
-    (ts, []) -> (ts, Nothing)
